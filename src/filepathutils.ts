66de9131b7d73dafc59cfcad909478239bee7961
Vadim Kantorov <vadimkantorov@gmail.com>
2025-02-16 14:41:44 +0100

    ...

--- src/filepathutils.ts
+++ src/filepathutils.ts
@@ -1,19 +1,19 @@
-export function dirname(path) {
+export function dirname(path : String) : String {
     if (!path)
         return '';
     return path.includes('/') ? path.slice(0, path.lastIndexOf('/')) : '';
 }
-export function basename(path) {
+export function basename(path : String) : String {
     if (!path)
         return '';
     return path.includes('/') ? path.substring(1 + path.lastIndexOf('/')) : path;
 }
-export function join2(path1, path2) {
+export function join2(path1 : String, path2: String): String {
     const path1_ = path1[path1.length - 1] == '/' ? path1.slice(0, path1.length - 1) : path1;
     const _path2 = path2[0] == '/' ? path2.substring(1) : path2;
     return (path1 && path2) ? (path1_ + '/' + _path2) : (path1 && !path2) ? path1 : (!path1 && path2) ? path2 : '';
 }
-export function sanitize_file_name(path) {
+export function sanitize_file_name(path: String): String {
     if (!path)
         return '';
     path = path.startsWith('/') ? path.substring(1) : path;
