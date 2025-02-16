e0e1afd5a92f1f738f6aa5093652f6ea55578c34
Vadim Kantorov <vadimkantorov@gmail.com>
2025-02-16 14:40:05 +0100

    ...

--- src/main.tsx
+++ src/main.tsx
@@ -13,36 +13,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App.tsx';
-
-function dirname(path)
-{
-    if(!path)
-        return '';
-    return path.includes('/') ? path.slice(0, path.lastIndexOf('/')) : '';
-}
-
-function basename(path)
-{
-    if(!path)
-        return '';
-    return path.includes('/') ? path.substring(1 + path.lastIndexOf('/')) : path;
-}
-
-function join2(path1, path2)
-{
-    const path1_ = path1[path1.length - 1] == '/' ? path1.slice(0, path1.length - 1) : path1
-    const _path2 = path2[0] == '/' ? path2.substring(1): path2;
-    return (path1 && path2) ? (path1_ + '/' + _path2) : (path1 && !path2) ? path1 : (!path1 && path2) ? path2 : ''
-}
-
-function sanitize_file_name(path)
-{
-    if(!path)
-        return '';
-    path = path.startsWith('/') ? path.substring(1) : path;
-    path = path.endsWith('/') ? path.slice(0, path.length - 1) : path;
-    return path;
-}
+import { dirname, join2 } from './filepathutils.ts';
 
 function update_location(path)
 {
