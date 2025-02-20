b87dcb3c74bee73e8e5e680953880c52c30dcf48
Vadim Kantorov <vadimkantorov@gmail.com>
2025-02-20 11:18:41 +0100

    ...

--- src/main.tsx
+++ src/main.tsx
@@ -37,7 +37,6 @@ import {parseAllowedColor, parseAllowedFontSize} from './styleConfig';
 
 import { join2 } from './filepathutils.ts';
 import { github_api_rename_file, github_api_get_file_dir, github_api_upsert_file, github_api_format_error, github_discover_url, github_api_prepare_params, github_api_update_file, github_api_get_file, github_api_signin, github_api_create_file, github_api_delete_file } from './github.ts';
-import { format_frontmatter, parse_frontmatter, update_frontmatter } from './frontmatter.ts';
 import { cache_load, cache_save } from './cacheutils.ts';
 
 const removeStylesExportDOM = (
@@ -182,6 +181,48 @@ const editorConfig = {
     nodes: [ParagraphNode, TextNode, HeadingNode, ListNode, ListItemNode],
     onError(error: Error) { throw error; },
 };
+
+
+function parse_frontmatter(text : string) : [string, Object]
+{
+    const m = text.match(/^---\n(.*?)\n---\n*/s);
+    let frontmatter = null;
+    if(m)
+    {
+        const frontmatter_str = m[1];
+        text = text.substring(m[0].length);
+
+        const procval = s => (s.length >= 2 && s[0] == '"' && s[s.length - 1] == '"') ? s.slice(1, s.length - 1) : (s.length >= 2 && s[0] == "'" && s[s.length - 1] == "'") ? s.slice(1, s.length - 1) : s;
+
+        for(const line of frontmatter_str.split('\n'))
+        {
+            const line_strip = line.trim();
+            const is_list_item = line_strip.startsWith('- ');
+            if(!line || line.startsWith('#'))
+                continue;
+
+            const colonIdx = line.indexOf(':');
+            const key = colonIdx != -1 ? line.slice(0, colonIdx).trim() : '';
+            const val = colonIdx != -1 ? line.substring(1 + colonIdx).trim() : is_list_item ? line_strip.substring(2).trim() : '';
+            
+            if(colonIdx != -1)
+            {
+                if(!frontmatter)
+                    frontmatter = {};
+                frontmatter[key] = val ? procval(val) : [];
+            }
+            else if(is_list_item)
+            {
+                if(!frontmatter)
+                    frontmatter = {};
+                if(!front_matter[key])
+                    frontmatter[key] = [];
+                frontmatter[key].push(procval(val));
+            }
+        }
+    }
+    return [text, frontmatter];
+}
 
 function App() {
     const editorRef = useRef(null);
@@ -473,7 +514,7 @@ function App() {
         }
     }
 
-    function onclick_addrow(event)
+    function onclick_frontmatter_addrow(event)
     {
         const html_frontmatter = document.getElementById('html_frontmatter');
         const html_row = event.target.parentElement.parentElement;
@@ -494,7 +535,7 @@ function App() {
             html_frontmatter.appendChild(html_header);
     }
 
-    function onclick_delrow(event)
+    function onclick_frontmatter_delrow(event)
     {
         const html_frontmatter = document.getElementById('html_frontmatter');
         const html_row = event.target.parentElement.parentElement;
@@ -507,6 +548,44 @@ function App() {
         }
         else
             html_frontmatter.deleteRow(rowIdx);
+    }
+
+    function format_frontmatter(html_frontmatter) : string 
+    {
+        const frontmatter_str_inside = Array.from(html_frontmatter.rows).filter(html_tr => html_tr.querySelectorAll('input')[0].value).map(html_tr => html_tr.querySelectorAll('input')[0].value + ': "' + html_tr.querySelectorAll('input')[1].value + '"').join('\n');
+        const frontmatter_str = `---\n${frontmatter_str_inside}\n---\n\n`;
+    
+        if (html_frontmatter.dataset.empty == 'false')
+            return frontmatter_str;
+    
+        return frontmatter_str_inside ? frontmatter_str : '';
+    }
+    
+    function update_frontmatter(html_frontmatter, frontmatter)
+    {
+        html_frontmatter.dataset.empty = frontmatter == null ? 'true' : 'false';
+    
+        const html_header = html_frontmatter.getElementsByTagName('tr')[0];
+        Array.from(html_header.getElementsByTagName('input')).forEach(input => input.value = '');
+        
+        const entries = Object.entries(frontmatter || {});
+        
+        let i = 0;
+        for(; i < entries.length; i++)
+        {
+            const [k, v] = entries[i];
+            let html_row = html_frontmatter.rows[1 + i];
+            if(html_row == null)
+            {
+                html_row = html_header.cloneNode(true);
+                html_frontmatter.appendChild(html_row);
+            }
+    
+            const [html_inputkey, html_inputval] = Array.from(html_row.getElementsByTagName('input'));
+            [html_inputkey.value, html_inputval.value] = [`${k}`, `${v}`];
+        }
+        for(let j = html_frontmatter.rows.length - 1; j > i; j--)
+            html_frontmatter.deleteRow(j);
     }
 
     function ondblclick_enter_file_tree(event)
@@ -598,7 +677,7 @@ function App() {
         if(url_value)
             open_file_or_dir(url_value, token_value);
         else
-            urlRef.focus();
+            urlRef.current.focus();
     }
   
   //const placeholder = 'Enter some rich text...';
@@ -614,7 +693,7 @@ function App() {
     </select>
     <table  hidden={isCompact} id="html_frontmatter">
         <tbody>
-            <tr><td><input type="text" placeholder="Frontmatter key:" /></td><td><input type="text" placeholder="Frontmatter value:" /></td><td><button onClick={onclick_addrow}>Add another row</button><button onClick={onclick_delrow}>Delete this row</button></td></tr>
+            <tr><td><input type="text" placeholder="Frontmatter key:" /></td><td><input type="text" placeholder="Frontmatter value:" /></td><td><button onClick={onclick_frontmatter_addrow}>Add another row</button><button onClick={onclick_frontmatter_delrow}>Delete this row</button></td></tr>
         </tbody>
     </table>
 
