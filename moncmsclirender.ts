669c7e9adc4830320b5b0b271173107507dfd5fa
Vadim Kantorov <vadimkantorov@gmail.com>
2025-02-22 17:47:38 +0100

    ...

---
+++ moncmsclirender.ts
@@ -0,0 +1,41 @@
+import { EmojiNode } from "./src/nodes/EmojiNode";
+
+import { TextNode, $getRoot, $selectAll } from "lexical";
+import { HeadingNode, QuoteNode } from "@lexical/rich-text";
+import { LinkNode } from "@lexical/link";
+import { HashtagNode } from "@lexical/hashtag";
+import { ListNode, ListItemNode } from "@lexical/list";
+
+import { createHeadlessEditor } from "@lexical/headless";
+import { $generateHtmlFromNodes } from "@lexical/html";
+import { JSDOM } from 'jsdom';
+
+let input : string = "", html : string = "";
+const editor = createHeadlessEditor({
+  editable: false,
+  nodes: [TextNode, HeadingNode, QuoteNode, LinkNode, HashtagNode, ListNode, ListItemNode, EmojiNode],
+  onError: (error) => { throw error;},
+});
+
+const dom = new JSDOM();
+global.window = dom.window;
+global.document = dom.window.document;
+
+process.stdin.on("data", (chunk) => {  input += chunk; });
+process.stdin.on("end", async () => 
+{
+  try
+  {
+    let editorStateJson = JSON.parse(input);
+    if('editorState' in editorStateJson) editorStateJson = editorStateJson.editorState;
+    const editorState = editor.parseEditorState(editorStateJson);
+    editor.setEditorState(editorState);    
+    editor.update(() => { html = $generateHtmlFromNodes(editor, $selectAll()); });
+    console.log(html);
+  }
+  catch (error)
+  {
+    console.error("Error processing EditorState JSON:", error);
+    process.exit(1);
+  }
+});
---
+++ svgdataurify.js
@@ -0,0 +1,14 @@
+let svg = "";
+
+process.stdin.on("data", (chunk) => {  svg += chunk; });
+process.stdin.on("end", async () => 
+{
+    const reWhitespace = /\s+/g, reUrlHexPairs = /%[\dA-F]{2}/g, hexDecode = {'%20': ' ', '%3D': '=', '%3A': ':', '%2F': '/'}, specialHexDecode = match => hexDecode[match] || match.toLowerCase();
+    if(svg.charCodeAt(0) === 0xfeff) svg = svg.slice(1);
+    svg = svg.trim().replace(reWhitespace, ' ').replaceAll('"', '\'');
+    svg = encodeURIComponent(svg);
+    svg = svg.replace(reUrlHexPairs, specialHexDecode);
+    const datauri = 'data:image/svg+xml,' + svg.replace(/ /g, '%20');
+    
+    console.log(datauri);
+});
