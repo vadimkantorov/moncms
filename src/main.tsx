603ecc22a7905842ff16e8149b6c5a2c6adbb36d
Vadim Kantorov <vadimkantorov@gmail.com>
2025-02-25 18:07:17 +0100

    ...

--- src/main.tsx
+++ src/main.tsx
@@ -38,102 +38,6 @@ import {parseAllowedColor, parseAllowedFontSize} from './styleConfig';
 
 import { github_api_rename_file, github_api_get_file_dir, github_api_upsert_file, github_api_format_error, github_api_prepare_params, github_api_update_file, github_api_get_file, github_api_signin, github_api_create_file, github_api_delete_file } from './github.ts';
 
-const removeStylesExportDOM = (
-  editor: LexicalEditor,
-  target: LexicalNode,
-): DOMExportOutput => {
-  const output = target.exportDOM(editor);
-  if (output && isHTMLElement(output.element)) {
-    // Remove all inline styles and classes if the element is an HTMLElement
-    // Children are checked as well since TextNode can be nested
-    // in i, b, and strong tags.
-    for (const el of [
-      output.element,
-      ...output.element.querySelectorAll('[style],[class],[dir="ltr"]'),
-    ]) {
-      el.removeAttribute('class');
-      el.removeAttribute('style');
-      if (el.getAttribute('dir') === 'ltr') {
-        el.removeAttribute('dir');
-      }
-    }
-  }
-  return output;
-};
-
-const exportMap: DOMExportOutputMap = new Map<
-  Klass<LexicalNode>,
-  (editor: LexicalEditor, target: LexicalNode) => DOMExportOutput
->([
-  [ParagraphNode, removeStylesExportDOM],
-  [TextNode, removeStylesExportDOM],
-]);
-
-const getExtraStyles = (element: HTMLElement): string => {
-  // Parse styles from pasted input, but only if they match exactly the
-  // sort of styles that would be produced by exportDOM
-  let extraStyles = '';
-  const fontSize = parseAllowedFontSize(element.style.fontSize);
-  const backgroundColor = parseAllowedColor(element.style.backgroundColor);
-  const color = parseAllowedColor(element.style.color);
-  if (fontSize !== '' && fontSize !== '15px') {
-    extraStyles += `font-size: ${fontSize};`;
-  }
-  if (backgroundColor !== '' && backgroundColor !== 'rgb(255, 255, 255)') {
-    extraStyles += `background-color: ${backgroundColor};`;
-  }
-  if (color !== '' && color !== 'rgb(0, 0, 0)') {
-    extraStyles += `color: ${color};`;
-  }
-  return extraStyles;
-};
-
-const constructImportMap = (): DOMConversionMap => {
-  const importMap: DOMConversionMap = {};
-
-  // Wrap all TextNode importers with a function that also imports
-  // the custom styles implemented by the playground
-  for (const [tag, fn] of Object.entries(TextNode.importDOM() || {})) {
-    importMap[tag] = (importNode) => {
-      const importer = fn(importNode);
-      if (!importer) {
-        return null;
-      }
-      return {
-        ...importer,
-        conversion: (element) => {
-          const output = importer.conversion(element);
-          if (
-            output === null ||
-            output.forChild === undefined ||
-            output.after !== undefined ||
-            output.node !== null
-          ) {
-            return output;
-          }
-          const extraStyles = getExtraStyles(element);
-          if (extraStyles) {
-            const {forChild} = output;
-            return {
-              ...output,
-              forChild: (child, parent) => {
-                const textNode = forChild(child, parent);
-                if ($isTextNode(textNode)) {
-                  textNode.setStyle(textNode.getStyle() + extraStyles);
-                }
-                return textNode;
-              },
-            };
-          }
-          return output;
-        },
-      };
-    };
-  }
-
-  return importMap;
-};
-
 const theme = {
   code: 'editor-code',
   heading: {
@@ -172,10 +76,6 @@ const theme = {
 
 const editorConfig = {
     theme: theme,
-    html: {
-        export: exportMap,
-        import: constructImportMap(),
-    },
     namespace: 'moncms',
     nodes: [ParagraphNode, TextNode, HeadingNode, ListNode, ListItemNode, CodeNode],
     onError(error: Error) { throw error; },
@@ -390,6 +290,7 @@ function App() {
         
         return window_editor_setMarkdown(msg);
     }
+    
     function onchange_files(event)
     {
         console.log(event);
